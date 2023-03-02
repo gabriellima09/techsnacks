41f326fa9d22c48b6e512178e6c50dc45312bed8
Gabriel Lima <gabriellimagomes.14@hotmail.com>
2023-03-02 18:00:26 -0300

    fixed tests

--- src/frontend/techsnacks-ui/src/app/app.component.ts
+++ src/frontend/techsnacks-ui/src/app/app.component.ts
@@ -10,7 +10,7 @@ import { DomSanitizer } from "@angular/platform-browser";
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'TeckSnacks';
+  title = 'TechSnacks';
 
   constructor(
     private matIconRegistry: MatIconRegistry,
